4fb6bafdbc97479f9c2a4e9a6b5bac7f06ebe727
Raymond Li <li.raymond04@gmail.com>
2025-06-28 16:46:04 -0600

    Simplify default keymap config

--- lua/ass-mpv/init.lua
+++ lua/ass-mpv/init.lua
@@ -5,9 +5,14 @@ local main_key = "<leader>m"
 
 M.setup = function(opts)
     opts = opts or {}
-    opts.keymaps = opts.keymaps or {}
-    opts.main_key = opts.main_key or main_key
     opts.use_main_key = opts.use_main_key or true
+    opts.main_key = opts.main_key or main_key
+    opts.keymaps = opts.keymaps
+        or {
+            mpv_open = opts.main_key .. "o",
+            mpv_close = opts.main_key .. "q",
+            mpv_pause = opts.main_key .. "p",
+        }
 
     -- Setup autocommands using the extracted module
     autocmds.setup_autocmds(opts)
--- lua/ass-mpv/keymaps.lua
+++ lua/ass-mpv/keymaps.lua
@@ -14,17 +14,15 @@ M.init_keymaps = function(bufnr, opts)
     end
   end
 
-  local main_key = opts.use_main_key and opts.main_key or ""
-
-  set_keymap(opts.keymaps.mpv_open or (main_key .. "o"), "Open MPV", function()
+  set_keymap(opts.keymaps.mpv_open, "Open MPV", function()
     vim.cmd("AssMpvOpen")
   end)
 
-  set_keymap(opts.keymaps.mpv_close or (main_key .. "q"), "Quit MPV", function()
+  set_keymap(opts.keymaps.mpv_close, "Quit MPV", function()
     mpv.quit_current()
   end)
 
-  set_keymap(opts.keymaps.mpv_pause or (main_key .. "p"), "Pause MPV", function()
+  set_keymap(opts.keymaps.mpv_pause, "Pause MPV", function()
     mpv.pause_current()
   end)
 end
